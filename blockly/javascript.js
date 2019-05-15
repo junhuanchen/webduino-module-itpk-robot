7e85a41e8c6089338cb3534774d1cd169fd7c437
Juwan <junhuanchen@qq.com>
2019-05-15 09:44:40 +0800

    add block code.

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -1,6 +1,6 @@
 Blockly.JavaScript['itpk_robot'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = itpk_create();;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
@@ -9,21 +9,21 @@ Blockly.JavaScript['itpk_ask'] = function(block) {
   var variable_itpk = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('itpk'), Blockly.Variables.NAME_TYPE);
   var value_question = Blockly.JavaScript.valueToCode(block, 'question', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = "{0}.itpk_ask({1});\n".format(variable_itpk, value_question);;
   return code;
 };
 
 Blockly.JavaScript['itpk_clear'] = function(block) {
   var variable_itpk = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('itpk'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = "{0}.answer()".format(variable_itpk);
   return code;
 };
 
 Blockly.JavaScript['itpk_answer'] = function(block) {
   var variable_itpk = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('itpk'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = "{0}.clear()".format(variable_itpk);
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
